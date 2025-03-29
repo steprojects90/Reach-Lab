2c6cf4159a2da45e5d540762847f4e040f7004be
steprojects90 <verrientistefano@gmail.com>
2025-03-29 11:43:29 +0100

    Update ReachLab.jsx

--- src/ReachLab.jsx
+++ src/ReachLab.jsx
@@ -133,7 +133,7 @@ const ReachLab = () => {
         midDensity = 0.01;
       } else if (targetSizeValue < 25000000) {
         // Target medio
-        midDensity = 0.010001;
+        midDensity = 0.010000000000001;
       } else {
         // Target grande
         midDensity = 0.01002;
